cf72014ff9e8bd5038fe226a3a896532206e1c73
sanavalos <santiago.avalos@outlook.com>
2025-01-24 22:53:35 -0300

    refactor: import clock container in app file

--- App.js
+++ App.js
@@ -1,40 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import moment from "moment-timezone";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import ClockContainer from "./components/ClockContainer";
 
 const App = () => {
-  const [selectedTimezone, setSelectedTimezone] = useState(moment.tz.guess());
-  const [currentTime, setCurrentTime] = useState(
-    moment().tz(selectedTimezone).format("HH:mm:ss")
-  );
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentTime(moment().tz(selectedTimezone).format("HH:mm:ss"));
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [selectedTimezone]);
-
-  const formatTimezone = (timezone) => {
-    const parts = timezone.split("/");
-    const partsCount = parts?.length;
-    if (partsCount > 1) {
-      const city = parts[partsCount - 1].replace("_", " ");
-      const country = parts[partsCount - 2].replace("_", " ");
-      return `${city}, ${country}`;
-    }
-    return timezone;
-  };
-
   return (
     <View style={styles.container}>
-      <View style={styles.bigClockContainer}>
-        <Text style={styles.timezoneName}>
-          {formatTimezone(selectedTimezone)}
-        </Text>
-        <Text style={styles.currentTime}>{currentTime}</Text>
-      </View>
+      <ClockContainer />
     </View>
   );
 };
@@ -46,19 +17,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     paddingVertical: 20,
-  },
-  bigClockContainer: {
-    alignItems: "center",
-    marginBottom: 20,
-  },
-  timezoneName: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 10,
-  },
-  currentTime: {
-    fontSize: 48,
-    fontWeight: "bold",
   },
 });
 
