1a62f000f1f7ff69c93a1603e82b64c28ad80f21
sanavalos <santiago.avalos@outlook.com>
2025-01-24 22:52:49 -0300

    refactor: create utils directory and formatters file

---
+++ utils/formatters.js
@@ -0,0 +1,10 @@
+export const formatTimezone = (timezone) => {
+  const parts = timezone.split("/");
+  const partsCount = parts?.length;
+  if (partsCount > 1) {
+    const city = parts[partsCount - 1].replace("_", " ");
+    const country = parts[partsCount - 2].replace("_", " ");
+    return `${city}, ${country}`;
+  }
+  return timezone;
+};
