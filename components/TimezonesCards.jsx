f47aeca00db54ab46918d39c1d9f14c13804c126
sanavalos <santiago.avalos@outlook.com>
2025-02-01 20:36:00 -0300

    feat: render time zone card component

--- components/TimezonesCards.jsx
+++ components/TimezonesCards.jsx
@@ -1,17 +1,15 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { styled } from "nativewind";
+import TimezoneCard from "./TimezoneCard";
 
 const StyledView = styled(View);
-const StyledText = styled(Text);
 
 function TimezonesCards({ timezoneList }) {
   return (
     <StyledView className="flex-wrap flex-row">
       {timezoneList.map((timezone) => (
-        <StyledView className="bg-gray-400 p-8 w-sm h-4 m-3 justify-center">
-          <StyledText>{timezone}</StyledText>
-        </StyledView>
+        <TimezoneCard timezone={timezone} />
       ))}
     </StyledView>
   );
