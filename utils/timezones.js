3b29f94a72483bcb17f4991a6be4c4f599b2cc9c
sanavalos <santiago.avalos@outlook.com>
2025-02-03 20:51:29 -0300

    feat: add timezone option list to utils

--- utils/timezones.js
+++ utils/timezones.js
@@ -1,1 +1,7 @@
 export const timezonesList = Intl.supportedValuesOf("timeZone");
+
+export const timezoneOptionList = [
+  { optionName: "Full Date-Time", formatName: "fullDateTime" },
+  { optionName: "Full Time", formatName: "fullTime" },
+  { optionName: "Hour-Minute Only", formatName: "hourMinuteOnly" }
+];
