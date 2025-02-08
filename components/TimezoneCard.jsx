8b58ddf237660018ffe7f2bf1f824f2e496c12af
sanavalos <santiago.avalos@outlook.com>
2025-02-08 13:04:49 -0300

    feat: utilize remove time zone from list function

--- components/TimezoneCard.jsx
+++ components/TimezoneCard.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import { styled } from "nativewind";
 import { getCurrentDateTimeForTimezone } from "../utils/formatters";
 import { useTimezoneStore } from "../store";
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
+const StyledButton = styled(Button);
 
 function TimezoneCard({ timezone }) {
   const timezoneOptionSelected = useTimezoneStore(
@@ -13,6 +14,10 @@ function TimezoneCard({ timezone }) {
   );
   const hourOptionSelected = useTimezoneStore(
     (state) => state.hourOptionSelected
+  );
+
+  const removeTimezoneFromList = useTimezoneStore(
+    (state) => state.removeTimezoneFromList
   );
 
   const [timezoneCurrentTime, setTimezoneCurrentTime] = useState(
@@ -38,7 +43,12 @@ function TimezoneCard({ timezone }) {
   }, [timezoneOptionSelected, hourOptionSelected]);
 
   return (
-    <StyledView className="bg-gray-400 p-8 w-sm h-4 m-3 justify-center">
+    <StyledView className="bg-gray-400 py-2 px-8 w-sm m-3 justify-center">
+      <StyledButton
+        onPress={() => removeTimezoneFromList(timezone)}
+        title="X"
+        accessibilityLabel="Remove time zone from list"
+      />
       <StyledText>{timezone}</StyledText>
       <StyledText>{timezoneCurrentTime}</StyledText>
     </StyledView>
