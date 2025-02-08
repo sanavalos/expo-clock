bb001ce0d64263aa792bc59ae46c197d6b5b9208
sanavalos <santiago.avalos@outlook.com>
2025-02-07 21:57:16 -0300

    feat: add hour option list logic to store

--- store.js
+++ store.js
@@ -1,11 +1,15 @@
 import { create } from "zustand";
-import { timezoneOptionList } from "./utils/timezones";
+import { timezoneOptionList, hourOptionList } from "./utils/timezones";
 
 export const useTimezoneStore = create((set) => ({
   timezoneOptionSelected: timezoneOptionList[0],
+  hourOptionSelected: hourOptionList[0],
   timezoneList: [],
   setTimezoneOptionSelected: (selectedOption) => {
     set(() => ({ timezoneOptionSelected: selectedOption }));
+  },
+  setHourOptionSelected: (selectedOption) => {
+    set(() => ({ hourOptionSelected: selectedOption }));
   },
   addTimezoneToList: (newTimezone) => {
     set((state) =>
